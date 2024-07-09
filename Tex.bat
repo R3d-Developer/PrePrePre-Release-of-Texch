5382b82e5c55a65d3bf53e6fcee3105dbdc8d50a
REDACTED <88716926+R3d-Developer@users.noreply.github.com>
2024-07-09 07:51:29 +0200

    Add files via upload

---
+++ Tex.bat
@@ -0,0 +1,497 @@
+@echo off 
+chcp 65001 >nul
+FOR /F "delims=: tokens=2" %%a in ('ipconfig ^| find "IPv4"') do set _IPAddress=%%a
+MODE CON COLS=120 LINES=9999
+:login
+cls
+color 7
+title [R3DACT3D]
+echo.
+echo.
+echo.
+echo			            .n                   .               .                  n.
+ping localhost -n 1>nul
+echo			     .     .dP                 dP                 9b                 9b.    .
+ping localhost -n 1>nul
+echo			    4      qXb          .     dX                   Xb        .       dXp     t
+ping localhost -n 1>nul
+echo			   dX.      9Xb      .dXb    __                     __       dXb.   dXP     .Xb
+ping localhost -n 1>nul
+echo			   9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ping localhost -n 1>nul
+echo			    9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+ping localhost -n 1>nul
+echo			     `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+ping localhost -n 1>nul
+echo			       `9XXXXXXXXXXXP' `9XX'   [4;91mBY[0m     `98v8P'    [4;91mRED[0m   `XXP' `9XXXXXXXXXXXP'
+ping localhost -n 1>nul
+echo			            ~~~~~~~      9X.          .dbIdb.          .XP       ~~~~~~~
+ping localhost -n 1>nul
+echo			                           )b.  .dbo.dP'`v'`9b.odb.  .dX(
+ping localhost -n 1>nul
+echo			                         ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+ping localhost -n 1>nul
+echo			                        dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+ping localhost -n 1>nul
+echo			                       dXXXXXXXXXXXXb   dIb   dXXXXXXXXXXXXb
+ping localhost -n 1>nul
+echo			                       9XXb'   `XXXXXb.dXIXb.dXXXXX'   `dXXP
+ping localhost -n 1>nul
+echo			                        `'      9XXXXXX(   )XXXXXXP      `'
+ping localhost -n 1>nul
+echo			                                 XXXX X.`v'.X XXXX
+ping localhost -n 1>nul
+echo			                                 XP^X'`b     d'`X^XX
+ping localhost -n 1>nul
+echo			                                 X. 9  `   '  P )X
+ping localhost -n 1>nul
+echo			                                 `b  `       '  d'
+ping localhost -n 1>nul																						
+echo 				  ▄▄▄█████▓ ▄▄▄      ▒██   ██▒ ▄████▄   ██░ ██		│[Info]: close Taxch with "c"
+echo 				  ▓  ██▒ ▓▒▒████▄    ▒▒ █ █ ▒░▒██▀ ▀█  ▓██░ ██░		│[91;4mguns.lol/godssonred[0m
+echo 				  ▒ ▓██░ ▒░▒██  ▀█▄  ░░  █   ░▒▓█    ▄ ▒██▀▀██░		│[91;4mgithub.com/R3d-Developer[0m
+echo 				  ░ ▓██▓ ░ ░██▄▄▄▄██  ░ █ █ ▒ ▒▓▓▄ ▄██▒░▓█ ░██		├───────────── 
+echo   				    ▒██▒ ░  ▓█   ▓██▒▒██▒ ▒██▒▒ ▓███▀ ░░▓█▒░██▓		│IP:
+echo 				    ▒ ░░    ▒▒   ▓▒█░▒▒ ░ ░▓ ░░ ░▒ ▒  ░ ▒ ░░▒░▒		│[100mNot tellin ya[0m
+echo  				      ░      ▒   ▒▒ ░░░   ░▒ ░  ░  ▒    ▒ ░▒░ ░		├─────────────
+echo  	    		            ░   ▒    ░    ░  ░         ░  ░░ ░			│Running on:
+echo  	        	    		     ░  ░ ░    ░  ░ ░       ░  ░  ░		│[4m%username%'s[0m PC
+echo		 			    		              ░                        
+echo.
+	echo  [97m╔══[0m([91m%username%[0m@[91m%computername%[0m)-[?]
+	echo  ║
+	set /p user=[97m ╚══$[0m
+		if %user% == c goto :cancel
+	echo  [97m╔══[0m([91m%username%[0m@[91m%computername%[0m)-[%user%]
+	echo  ║
+	set /p pass=[97m ╚══$[0m
+		if %pass% == c goto :cancel
+		if %user% == admin if %pass% == admin goto :Main
+			goto FALSCH
+:FALSCH
+title KILL URSELF
+cls
+color 4
+echo █▓▒▓█▀██▀█▄░░▄█▀██▀█▓▒▓█
+echo █▓▒░▀▄▄▄▄▄█░░█▄▄▄▄▄▀░▒▓█
+echo █▓▓▒░░░░░▒▓░░▓▒░░░░░▒▓▓█
+echo.
+echo KILL URSELF or read "README"
+timeout 3 >nul
+exit
+:Main
+color 7
+MODE CON COLS=120 LINES=9999
+title Taxch"Tek-sch" Tool's
+cls
+echo.
+echo.
+echo.
+echo			            .n                   .               .                  n.
+ping localhost -n 1>nul
+echo			     .     .dP                 dP                 9b                 9b.    .
+ping localhost -n 1>nul
+echo			    4      qXb          .     dX                   Xb        .       dXp     t
+ping localhost -n 1>nul
+echo			   dX.      9Xb      .dXb    __                     __       dXb.   dXP     .Xb
+ping localhost -n 1>nul
+echo			   9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ping localhost -n 1>nul
+echo			    9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+ping localhost -n 1>nul
+echo			     `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+ping localhost -n 1>nul
+echo			       `9XXXXXXXXXXXP' `9XX'   [4;91mBY[0m     `98v8P'    [4;91mRED[0m   `XXP' `9XXXXXXXXXXXP'
+ping localhost -n 1>nul
+echo			            ~~~~~~~      9X.          .dbIdb.          .XP       ~~~~~~~
+ping localhost -n 1>nul
+echo			                           )b.  .dbo.dP'`v'`9b.odb.  .dX(
+ping localhost -n 1>nul
+echo			                         ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+ping localhost -n 1>nul
+echo			                        dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+ping localhost -n 1>nul
+echo			                       dXXXXXXXXXXXXb   dIb   dXXXXXXXXXXXXb
+ping localhost -n 1>nul
+echo			                       9XXb'   `XXXXXb.dXIXb.dXXXXX'   `dXXP
+ping localhost -n 1>nul
+echo			                        `'      9XXXXXX(   )XXXXXXP      `'
+ping localhost -n 1>nul
+echo			                                 XXXX X.`v'.X XXXX
+ping localhost -n 1>nul
+echo			                                 XP^X'`b     d'`X^XX
+ping localhost -n 1>nul
+echo			                                 X. 9  `   '  P )X
+ping localhost -n 1>nul
+echo			                                 `b  `       '  d'
+ping localhost -n 1>nul																						
+echo                                                        [91m░░░░░[0m
+ping localhost -n 1>nul
+echo                                                       [91m░░░░░░░[0m
+ping localhost -n 1>nul
+echo                                                       [91m░░░░░░░[0m
+ping localhost -n 1>nul
+echo                                                       [91m░░░░░░░[0m
+ping localhost -n 1>nul
+echo                                 Head Developer       [91m░░░░░░░░░[0m       REDACTED
+ping localhost -n 1>nul
+echo                   [91m░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░[0m
+ping localhost -n 1>nul
+echo                 [91m░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░[0m
+ping localhost -n 1>nul
+echo ╔═══                         ═══╗        ╔═══         [ADMIN]         ═══╗        ╔═══                         ═══╗  
+ping localhost -n 1>nul
+echo ║   [1] Colour.bat              ║        ║   [10] N/A                    ║        ║   [19] N/A                    ║
+ping localhost -n 1>nul
+echo     [2] Hide.bat                             [11] N/A                                 [21] N/A
+ping localhost -n 1>nul
+echo     [3] N/A                                  [12] N/A                                 [22] N/A
+ping localhost -n 1>nul
+echo     [4] N/A                                  [13] N/A                                 [23] N/A
+ping localhost -n 1>nul
+echo     [5] N/A                                  [14] N/A                                 [24] N/A
+ping localhost -n 1>nul
+echo     [6] N/A                                  [15] N/A                                 [25] N/A
+ping localhost -n 1>nul
+echo     [7] N/A                                  [16] N/A                                 [26] N/A
+ping localhost -n 1>nul
+echo     [8] Encrypt                              [17] N/A                                 [27] N/A
+ping localhost -n 1>nul
+echo ║   [9] Decrypt                 ║        ║   [18] N/A                    ║        ║   [C] Close Application       ║
+ping localhost -n 1>nul
+echo ╚═══                         ═══╝        ╚═══                         ═══╝        ╚═══                         ═══╝
+echo.
+echo.
+echo.
+echo.
+echo.
+echo  [97m╔══[0m([91m%username%[0m@[91m%computername%[0m)-[%user%/%pass%]
+echo  ║
+set /p cmd=[97m ╚══$[0m
+
+if %cmd% == c goto :cancel
+if %cmd% == 1 goto :colourr
+if %cmd% == 2 goto :Hidee
+if %cmd% == 3 goto :Arsch
+if %cmd% == 4 goto :Arsch
+if %cmd% == 5 goto :Arsch
+if %cmd% == 6 goto :Arsch
+if %cmd% == 7 goto :Arsch
+if %cmd% == 8 goto :Encrypt
+if %cmd% == 9 goto :Decrypt
+if %cmd% == 10 goto :ahah
+if %cmd% == 11 goto :ahah
+if %cmd% == 12 goto :ahah
+if %cmd% == 13 goto :ahah
+if %cmd% == 14 goto :ahah
+if %cmd% == 15 goto :ahah
+if %cmd% == 16 goto :ahah
+if %cmd% == 17 goto :ahah
+if %cmd% == 18 goto :ahah
+if %cmd% == 19 goto
+if %cmd% == 20 goto
+if %cmd% == 21 goto
+if %cmd% == 22 goto
+if %cmd% == 23 goto
+if %cmd% == 24 goto
+if %cmd% == 25 goto
+if %cmd% == 26 goto
+if %cmd% == 27 goto
+
+goto :Arsch
+
+:colourr
+MODE CON COLS=120 LINES=9999
+title Color.bat by [REDACTED]
+ping localhost -n 1>nul
+cls
+ping localhost -n 1>nul
+echo [101;93m STYLES [0m
+ping localhost -n 1>nul
+echo ^<ESC^>[0m [0mReset[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[1m [1mBold[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[4m [4mUnderline[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[7m [7mInverse[0m
+ping localhost -n 1>nul
+echo.
+ping localhost -n 1>nul
+echo [101;93m NORMAL FOREGROUND COLORS [0m
+ping localhost -n 1>nul
+echo ^<ESC^>[30m [30mBlack[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[31m [31mRed[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[32m [32mGreen[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[33m [33mYellow[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[34m [34mBlue[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[35m [35mMagenta[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[36m [36mCyan[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[37m [37mWhite[0m
+ping localhost -n 1>nul
+echo.
+ping localhost -n 1>nul
+echo [101;93m NORMAL BACKGROUND COLORS [0m
+ping localhost -n 1>nul
+echo ^<ESC^>[40m [40mBlack[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[41m [41mRed[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[42m [42mGreen[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[43m [43mYellow[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[44m [44mBlue[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[45m [45mMagenta[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[46m [46mCyan[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[47m [47mWhite[0m
+ping localhost -n 1>nul
+echo.
+ping localhost -n 1>nul
+echo [101;93m STRONG FOREGROUND COLORS [0m
+ping localhost -n 1>nul
+echo ^<ESC^>[90m [90mBlack[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[91m [91mRed[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[92m [92mGreen[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[93m [93mYellow[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[94m [94mBlue[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[95m [95mMagenta[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[96m [96mCyan[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[97m [97mWhite[0m
+ping localhost -n 1>nul
+echo.
+ping localhost -n 1>nul
+echo [101;93m STRONG FOREGROUND COLORS [0m
+ping localhost -n 1>nul
+echo ^<ESC^>[100m [100mBlack[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[101m [101mRed[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[102m [102mGreen[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[103m [103mYellow[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[104m [104mBlue[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[105m [105mMagenta[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[106m [106mCyan[0m
+ping localhost -n 1>nul
+echo ^<ESC^>[107m [107mWhite[0m
+ping localhost -n 1>nul
+echo.
+echo.
+echo made by [4m[[91mRED[0m[4mACTED][0m
+set /p close=[F]:
+
+if %close% == Superkalifragilistikexpialigetisch goto secret
+if %close% == c (goto Main) else (goto ohno)
+
+:ohno
+cls
+echo Hurensohn drück "c" um zu leaven weil hab mit Else statement rumgetopbt und mach das nicht raus 
+pause
+goto start
+
+:secret
+echo fühlst du dich jetzt besonders oder was?
+echo (Y/N)?
+set /p frage=[?]:
+
+if %frage% == Y goto Yes
+if %frage% == N goto No
+
+:Yes
+title dont care 
+cls
+echo still dont care
+echo wirklich
+echo jetzt hau ab.
+timeout 2 >nul
+exit
+
+:No
+cls
+color 4
+title SUPERKALIFRAGILISTIKEXPIALIGETISCH
+:loop
+echo SUPERKALIFRAGILISTIKEXPIALIGETISCH
+goto loop
+
+:Hidee
+cls
+(
+echo @echo off
+echo echo [31mPass auf was du jetzt drückst und entferne den code lieber der dir die nachricht gibt[0m
+echo pause
+echo cd %%temp%%
+echo mkdir 3f12klru.pz2
+echo cd %%temp%%\3f12klru.pz2
+echo mkdir 31082
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo mkdir %%random%%
+echo cd %%~dp0
+echo copy AH.bat %%temp%%\3f12klru.pz2\31082
+echo :: copy [YOUR MALWARE] %%temp%%\3f12klru.pz2\31082
+echo cd %%~dp0
+echo :: reg add HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run /v test /t reg_expand_sz /d %%temp%%\3f12klru.pz2\31082\[YOUR MALWARE]
+echo del AH.bat
+)>>"AH.bat"
+echo AH.bat wurde zu %~dp0 hinzugefügt.
+echo Kehre zum Hauptmenue zurueck
+timeout 3 >nul
+goto :Main
+
+
+:Encrypt
+@echo off
+cls
+setlocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+title Encrypt
+color 4
+:mainmenu
+set savefile=on
+::set Encrypt=Nothing
+(set CHAR[a]=UDFM45) & (set CHAR[b]=H21DGF) & (set CHAR[c]=FDH56D) & (set CHAR[d]=FGS546) & (set CHAR[e]=JUK4JH)
+(set CHAR[f]=ERG54S) & (set CHAR[g]=T5H4FD) & (set CHAR[h]=RG641G) & (set CHAR[i]=RG4F4D) & (set CHAR[j]=RT56F6)
+(set CHAR[k]=VCBC3B) & (set CHAR[l]=F8G9GF) & (set CHAR[m]=FD4CJS) & (set CHAR[n]=G423FG) & (set CHAR[o]=F45GC2)
+(set CHAR[p]=TH5DF5) & (set CHAR[q]=CV4F6R) & (set CHAR[r]=XF64TS) & (set CHAR[s]=X78DGT) & (set CHAR[t]=TH74SJ)
+(set CHAR[u]=BCX6DF) & (set CHAR[v]=FG65SD) & (set CHAR[w]=4KL45D) & (set CHAR[x]=GFH3F2) & (set CHAR[y]=GH56GF)
+(set CHAR[z]=45T1FG) & (set CHAR[1]=D4G23D) & (set CHAR[2]=GB56FG) & (set CHAR[3]=SF45GF) & (set CHAR[4]=P4FF12)
+(set CHAR[5]=F6DFG1) & (set CHAR[6]=56FG4G) & (set CHAR[7]=USGFDG) & (set CHAR[8]=FKHFDG) & (set CHAR[9]=IFGJH6)
+(set CHAR[0]=87H8G7) & (set CHAR[ä]=G25GHF) & (set CHAR[ö]=45FGFH) & (set CHAR[ü]=75FG45) & (set CHAR[*]=54GDH5)
+(set CHAR[(]=45F465) & (set CHAR[.]=HG56FG) & (set CHAR[,]=DF56H4) & (set CHAR[-]=F5JHFH) & (set CHAR[ ]=SGF4HF)
+(set CHAR[\]=45GH45) & (set CHAR[/]=56H45G)
+echo Enter a string to encrypt:
+set /p Encrypt=
+cls
+set Encrypt2=%Encrypt%
+set "EncryptOut="
+:encrypt2
+set char=%Encrypt2:~0,1%
+set Encrypt2=%Encrypt2:~1%
+set EncryptOut=%EncryptOut%!CHAR[%char%]!
+if not "%Encrypt2%"=="" goto encrypt2
+echo Input string: %Encrypt%
+echo.
+echo Output string: %EncryptOut%
+set string=%EncryptOut%
+set temp_str=%string%
+set str_len=0
+:lengthloop
+if defined temp_str (
+set temp_str=%temp_str:~1%
+set /A str_len += 1
+goto lengthloop )
+echo.
+echo Output string is %str_len% characters long!
+if "%savefile%"=="on" echo.%EncryptOut%>>%~d0%~p0encrypted.txt
+echo.
+pause
+cls
+goto Main
+
+
+:Decrypt
+@echo off
+cls
+setlocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+title Decrypt
+color 4
+:mainmenu2
+set savefile=on
+::set Decrypt=Nothing
+(set CHAR[UDFM45]=a) & (set CHAR[H21DGF]=b) & (set CHAR[FDH56D]=c) & (set CHAR[FGS546]=d) & (set CHAR[JUK4JH]=e)
+(set CHAR[ERG54S]=f) & (set CHAR[T5H4FD]=g) & (set CHAR[RG641G]=h) & (set CHAR[RG4F4D]=i) & (set CHAR[RT56F6]=j)
+(set CHAR[VCBC3B]=k) & (set CHAR[F8G9GF]=l) & (set CHAR[FD4CJS]=m) & (set CHAR[G423FG]=n) & (set CHAR[F45GC2]=o)
+(set CHAR[TH5DF5]=p) & (set CHAR[CV4F6R]=q) & (set CHAR[XF64TS]=r) & (set CHAR[X78DGT]=s) & (set CHAR[TH74SJ]=t)
+(set CHAR[BCX6DF]=u) & (set CHAR[FG65SD]=v) & (set CHAR[4KL45D]=w) & (set CHAR[GFH3F2]=x) & (set CHAR[GH56GF]=y)
+(set CHAR[45T1FG]=z) & (set CHAR[D4G23D]=1) & (set CHAR[GB56FG]=2) & (set CHAR[SF45GF]=3) & (set CHAR[P4FF12]=4)
+(set CHAR[F6DFG1]=5) & (set CHAR[56FG4G]=6) & (set CHAR[USGFDG]=7) & (set CHAR[FKHFDG]=8) & (set CHAR[IFGJH6]=9)
+(set CHAR[87H8G7]=0) & (set CHAR[G25GHF]=ä) & (set CHAR[45FGFH]=ö) & (set CHAR[75FG45]=ü) & (set CHAR[54GDH5]=*)
+(set CHAR[45F465]=() & (set CHAR[HG56FG]=.) & (set CHAR[DF56H4]=,) & (set CHAR[F5JHFH]=-) & (set CHAR[SGF4HF]= )
+(set CHAR[45GH45]=\) & (set CHAR[56H45G]=/)
+echo Enter a string to decrypt:
+set /p Decrypt=
+cls
+set Decrypt2=%Decrypt%
+set "DecryptOut="
+:decrypt2
+set char=%Decrypt2:~0,6%
+set Decrypt2=%Decrypt2:~6%
+set DecryptOut=%DecryptOut%!CHAR[%char%]!
+if not "%Decrypt2%"=="" goto decrypt2
+echo Input string: %Decrypt%
+echo.
+echo Output string: %DecryptOut%
+set string=%DecryptOut%
+set temp_str=%string%
+set str_len=0
+:lengthloop
+if defined temp_str (
+set temp_str=%temp_str:~1%
+set /A str_len += 1
+goto lengthloop )
+echo.
+echo Output string is %str_len% characters long!
+if "%savefile%"=="on" echo.%DecryptOut%>>%~d0%~p0decrypted.txt
+echo.
+pause
+cls
+goto Main
+
+:Arsch
+echo Gibts (noch) nicht Hurensohn
+timeout 3 >nul
+exit
+
+:ahah
+[31;4mDu hast keine Admin berechtigung du Loser![0m
+pause
+exit
+
+:cancel 
+exit
+ 
